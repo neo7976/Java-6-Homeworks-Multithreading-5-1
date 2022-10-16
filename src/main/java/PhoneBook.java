7adeccaa0e1dbb633c872918435d57cf64caeb8d
Dmitrii Sobin <neo7976@yandex.ru>
2022-10-16 13:40:15 +0300

    Stub and test for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,7 @@
 public class PhoneBook {
+
+    public boolean add(String name, String number) {
+        return false;
+
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,9 +10,21 @@ class PhoneBookTest {
 
     @BeforeEach
     void setUp() {
+        System.out.println("Начало теста");
+        phoneBook = new PhoneBook();
     }
 
     @AfterEach
     void tearDown() {
+        System.out.println("Окончание теста");
+        phoneBook = null;
+    }
+
+    @Test
+    void add_test() {
+        String name = "Вася";
+        String number = "+7(905) 968-12-25";
+        int expected = 1;
+        Assertions.assertEquals(expected, phoneBook.add(name, number));
     }
 }
